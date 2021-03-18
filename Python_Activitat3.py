bd12c990f0ebbf50a28ba923fda41ac0764f5cd6
abohera <abohera@institutmvm.cat>
2021-03-18 10:27:41 +0100

    Second Commit

--- Python_Activitat3.py
+++ Python_Activitat3.py
@@ -1,8 +1,18 @@
 def llista():
+# El usuario introduce por teclado un número del 1 al 20
+    num = int(input("Introduce un número: "))
+# Validamos si se ha introducido un número del 1 al 20
+    while num > 20 or num < 1:
+        num = int(input("Introduce un número del 1 al 20: "))
+    return num
 
+# Se printa el resultado de la operación
+def multiplos(num):
+    print([num * x for x in range(100) if (num * x) < 100])
 
 def main():
-    llista()
+    n = llista()
+    multiplos(n)
 
 if __name__ == "__main__":
    main()
